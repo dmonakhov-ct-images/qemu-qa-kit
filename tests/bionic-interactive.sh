2c7bd756e3fb510f473dff0096b2f65391100de1
Dmitry Monakhov <dmtrmonakhov@yandex-team.ru>
2018-10-18 11:02:21 +0300

    add interactive \example

---
+++ tests/bionic-interactive.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+
+TEST_NAME=bionic-server-cloudimg-amd64
+IMAGE_NAME=image.qcow2
+IMAGE_URL=http://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
+
+mkdir -p work/$TEST_NAME
+cat <<EOF > work/$TEST_NAME/cfg_seed.yaml
+repo_update: true
+packages:
+  - emacs-nox
+  - gdisk
+  - lvm
+EOF
+
+[ -e "work/$TEST_NAME/$IMAGE_NAME" ] || curl -o work/$TEST_NAME/$IMAGE_NAME $IMAGE_URL
+bash -xe $DIR/../docker-run.sh -it -W $(pwd)/work/$TEST_NAME -- -G -n 2 -m 512M
